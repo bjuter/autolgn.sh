b7da953b12c3842ee744f7783dad3d85d39c3dfd
son <son@tianbayun.com>
2018-09-05 13:07:35 +0800

    suppress progress bar

--- autolgn.sh
+++ autolgn.sh
@@ -9,7 +9,7 @@ DATE=`date '+%Y-%m-%d %H:%M:%S'`
 user='<username>'
 pwd='<password>'
 
-r=`curl 'https://lgn.bjut.edu.cn/' --data "DDDDD=$user&v46s=1&v6ip=&0MKKey=&upass=$pwd" | grep 'You have successfully logged into our system.'`
+r=`curl 'https://lgn.bjut.edu.cn/' --data "DDDDD=$user&v46s=1&v6ip=&0MKKey=&upass=$pwd" 2> /dev/null | grep 'You have successfully logged into our system.'`
 
 if [ "${r}" == '' ]; then
   echo ${DATE} Failed
